af2d53bf462eae2c14e78cb1e9626a0b0c7ce8b6
KwonSunwon <55453462+KwonSunwon@users.noreply.github.com>
2022-03-02 10:45:31 +0900

    Add files via upload

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+void main() {
+	char input[60];
+	char order;
+
+	std::cout << "Input sentenc : ";
+	gets_s(input, sizeof(input));
+
+	std::cout << input;
+
+	switch (order)
+	{
+	case 'e' : 
+	default:
+		break;
+	}
+}
