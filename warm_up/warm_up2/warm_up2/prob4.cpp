a4a45f07d3cc815cce7990d411f91e161b59b383
KwonSunwon <kso00011147@gmail.com>
2022-03-11 01:44:47 +0900

    o

--- warm_up/warm_up2/warm_up2/prob4.cpp
+++ warm_up/warm_up2/warm_up2/prob4.cpp
@@ -4,14 +4,15 @@ const int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 const char *week[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saturday"};
 
 void dateOfWeek(int year, int month, int day);
+void showCalendar(int year, int month);
 
-int main()
+int main(void)
 {
     int year;
     int month;
     int day;
 
-    scanf_s("%d/%d/%d", &year, &month, &day);
+    scanf("%d/%d/%d", &year, &month, &day);
 
     dateOfWeek(year, month, day);
 
@@ -19,10 +20,22 @@ int main()
     {
     case 1:
         break;
-
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
     default:
         break;
     }
+
+    return 0;
 }
 
 void dateOfWeek(int year, int month, int day)
@@ -41,4 +54,8 @@ void dateOfWeek(int year, int month, int day)
     dayNumber += day;
 
     std::cout << week[dayNumber % 7];
+}
+
+void showCalendar(int year, int month)
+{
 }
