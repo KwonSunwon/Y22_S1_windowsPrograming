a9c6eea6b242d556161187fc1713815f7b25a147
KwonSunwon <kso00011147@gmail.com>
2022-03-13 02:37:03 +0900

    문제5 해결

--- warm_up/warm_up2/warm_up2/prob5.cpp
+++ warm_up/warm_up2/warm_up2/prob5.cpp
@@ -1,11 +1,89 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
+typedef struct _OPER
+{
+    int prior;
+    char op;
+} OPER;
+
+float calculate(stack<float> *num, stack<OPER> *oper);
+
 int main(void)
 {
-    int num[5];
-    char oper[4];
+    char input[14] = {'\0'};
+    stack<float> num;
+    stack<OPER> oper;
 
-    scanf("%d%c%d%c%d%c%d%c%d", &num[0], &oper[0], &num[1], &oper[1], &num[2], &oper[2], &num[3], &oper[3], &num[4], &oper[4], &num[5]);
+    cin >> input;
+
+    float temp = 0;
+    int i = 0;
+    int prior = 0;
+
+    while (input[i] != '\0')
+    {
+        if (input[i] > 47 && input[i] < 58)
+        {
+            temp = temp * 10 + (input[i] - 48);
+            ++i;
+        }
+        else
+        {
+            num.push(temp);
+            temp = 0;
+            switch (input[i])
+            {
+            case '+':
+            case '-':
+                prior = 1;
+                break;
+            case '*':
+            case '/':
+                prior = 2;
+                break;
+            }
+            while (!oper.empty() && prior <= oper.top().prior)
+            {
+                num.push(calculate(&num, &oper));
+            }
+            oper.push({prior, input[i]});
+            ++i;
+        }
+    }
+
+    num.push(temp);
+
+    while (!oper.empty())
+    {
+        num.push(calculate(&num, &oper));
+    }
+
+    cout << input << " = " << num.top();
+}
+
+float calculate(stack<float> *num, stack<OPER> *oper)
+{
+    float n1 = num->top();
+    num->pop();
+    float n2 = num->top();
+    num->pop();
+
+    switch (oper->top().op)
+    {
+    case '+':
+        oper->pop();
+        return n2 + n1;
+    case '-':
+        oper->pop();
+        return n2 - n1;
+    case '*':
+        oper->pop();
+        return n2 * n1;
+    case '/':
+        oper->pop();
+        return n2 / n1;
+    }
 }
