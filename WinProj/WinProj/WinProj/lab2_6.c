64120042ae1bdf95519dfa1e7cd9c9d449d0f8fa
KwonSunwon <kso00011147@gmail.com>
2022-03-28 23:43:36 +0900

    6번 해결
    x, 나비 모양만 조금 수정하면 될듯?

--- WinProj/WinProj/WinProj/lab2_6.c
+++ WinProj/WinProj/WinProj/lab2_6.c
@@ -1,14 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+// debuging console
+
 #include <Windows.h>
 #include <tchar.h>
 #include <time.h>
+
+#include <stdio.h>
 
 HINSTANCE g_hInst;
 LPCTSTR lpszClass = L"Window Class Name";
 LPCTSTR lpszWindowName = L"Window Programming Lab";
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+void drawX(int, TCHAR *);
+void drawRevTriangle(int, TCHAR *);
+void drawRhombus(int, TCHAR *);
+void drawButterfly(int, TCHAR *);
+void drawHourglass(int, TCHAR *);
+void drawRectangle(int, TCHAR *);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
 {
@@ -49,23 +61,83 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
     PAINTSTRUCT ps;
     HDC hDC;
 
-    static int row, col;
+    static int row, col, x, y;
     static int colorCode[3];
 
-    static RECT rect;
+    static RECT rect[50];
+
+    static TCHAR *str = L"1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9\r\n10\r\n11\r\n12\r\n13\r\n14\r\n15\r\n16\r\n17\r\n18\r\n19\r\n20";
+
+    static TCHAR *strTest = L"This is passible?\r\nEnter";
 
     switch (iMessage)
     {
     case WM_CREATE:
         srand(time(NULL));
-        row = rand() % 8 + 2;
-        col = rand() % 8 + 2;
-        for (int i = 0; i < 3; ++i)
-            colorCode[i] = rand() % 255;
+        row = rand() % 4 + 2;
+        col = rand() % 4 + 2;
+        // row = 5;
+        // col = 5;
+
+        x = 800 / col;
+        y = 600 / row;
+
+        // rect = (RECT *)malloc(row * col); -이유는 파악하지 못 했는데 동적할당을 하면 출력할 때 이상한 값이 들어감
+        //  + 동적할당 해제할 위치를 잘 모르겠음(아마 윈도우가 죽을 때나 출력이 끝나고 나면 free해주면 될거 같은데 계속 오류 발생함)
+
+        int idx = 0;
+        for (int i = 0; i < col; ++i)
+            for (int j = 0; j < row; ++j)
+            {
+                rect[idx].left = x * i;
+                rect[idx].right = x * (i + 1);
+                rect[idx].top = y * j;
+                rect[idx].bottom = y * (j + 1);
+                // printf("입력 rect[%d] (%d, %d, %d, %d)\n", idx, rect[idx].left, rect[idx].right, rect[idx].top, rect[idx].bottom);
+                ++idx;
+            }
+
+        printf("col : %d, row : %d\nx : %d, y : %d\n", col, row, x, y);
+
         break;
 
     case WM_PAINT:
         hDC = BeginPaint(hWnd, &ps);
+
+        for (int i = 0; i < row * col; ++i)
+        {
+            int randDraw = rand() % 6;
+            // randDraw = 5;
+            TCHAR drawStr[1000] = {'\0'};
+            switch (randDraw)
+            {
+            case 0:
+                drawX(x / 20, drawStr);
+                break;
+            case 1:
+                drawRevTriangle(x / 20, drawStr);
+                break;
+            case 2:
+                drawRhombus(x / 20, drawStr);
+                break;
+            case 3:
+                drawButterfly(x / 20, drawStr);
+                break;
+            case 4:
+                drawHourglass(x / 20, drawStr);
+                break;
+            case 5:
+                drawRectangle(x / 20, drawStr);
+                break;
+            }
+            for (int i = 0; i < 3; ++i)
+                colorCode[i] = rand() % 255;
+            SetBkColor(hDC, RGB(colorCode[0], colorCode[1], colorCode[2]));
+            SetTextColor(hDC, RGB(colorCode[1], colorCode[2], colorCode[1]));
+            // SetTextColor(hDC, RGB(colorCode[0], colorCode[1], colorCode[2]));
+            DrawText(hDC, drawStr, lstrlen(drawStr), &rect[i], DT_CENTER);
+            // printf("출력 rect[%d] (%d, %d, %d, %d)\n", i, rect[i].left, rect[i].right, rect[i].top, rect[i].bottom);
+        }
 
         EndPaint(hWnd, &ps);
         break;
@@ -78,4 +150,181 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
     }
 
     return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+void drawX(int width, TCHAR *str)
+{
+    int blank = 0;
+    int blank2 = width / 2;
+
+    while (blank2 > 1)
+    {
+        for (int i = 0; i < blank; ++i)
+            _tcscat(str, L" ");
+        _tcscat(str, L"*");
+        for (int i = 0; i < blank2; ++i)
+            _tcscat(str, L" ");
+        _tcscat(str, L"*\n");
+
+        ++blank;
+        blank2 -= 2;
+    }
+
+    while (blank2 <= width / 2)
+    {
+        for (int i = 0; i < blank; ++i)
+            _tcscat(str, L" ");
+        _tcscat(str, L"*");
+        for (int i = 0; i < blank2; ++i)
+            _tcscat(str, L" ");
+        _tcscat(str, L"*\n");
+
+        --blank;
+        blank2 += 2;
+    }
+}
+void drawRevTriangle(int width, TCHAR *str)
+{
+    int blank = 0;
+    for (int i = 0; i < (width / 2); ++i)
+    {
+        for (int j = 0; j < blank; ++j)
+        {
+            _tcscat(str, L" ");
+        }
+        for (int k = 0; k < (width - blank * 2); ++k)
+        {
+            _tcscat(str, L"*");
+        }
+        _tcscat(str, L"\n");
+        ++blank;
+    }
+}
+void drawRhombus(int width, TCHAR *str)
+{
+    int blank = width / 2 - 1;
+    int asterisk = 2;
+
+    while (asterisk < width)
+    {
+        for (int i = 0; i < blank; ++i)
+            _tcscat(str, L" ");
+        for (int i = 0; i < asterisk; ++i)
+            _tcscat(str, L"*");
+        _tcscat(str, L"\n");
+        blank -= 2;
+        asterisk += 4;
+    }
+
+    while (asterisk >= 2)
+    {
+        for (int i = 0; i < blank; ++i)
+            _tcscat(str, L" ");
+        for (int i = 0; i < asterisk; ++i)
+            _tcscat(str, L"*");
+        _tcscat(str, L"\n");
+        blank += 2;
+        asterisk -= 4;
+    }
+}
+void drawButterfly(int width, TCHAR *str)
+{
+    int blank = width - 2;
+    int asterisk = 1;
+
+    while (asterisk < width / 2)
+    {
+        for (int i = 0; i < asterisk; ++i)
+            _tcscat(str, L"*");
+        for (int i = 0; i < blank; ++i)
+            _tcscat(str, L" ");
+        for (int i = 0; i < asterisk; ++i)
+            _tcscat(str, L"*");
+        _tcscat(str, L"\n");
+        blank -= 4;
+        asterisk += 2;
+    }
+
+    if (asterisk != width / 2)
+    {
+        for (int i = 0; i < (asterisk - 1) * 2; ++i)
+            _tcscat(str, L"*");
+        _tcscat(str, L"\n");
+        blank += 4;
+        asterisk -= 2;
+    }
+
+    while (asterisk > 0)
+    {
+        for (int i = 0; i < asterisk; ++i)
+            _tcscat(str, L"*");
+        for (int i = 0; i < blank; ++i)
+            _tcscat(str, L" ");
+        for (int i = 0; i < asterisk; ++i)
+            _tcscat(str, L"*");
+        _tcscat(str, L"\n");
+        blank += 4;
+        asterisk -= 2;
+    }
+}
+void drawHourglass(int width, TCHAR *str)
+{
+    int asterisk = width;
+    int blank = 0;
+
+    while (asterisk > 2)
+    {
+        for (int i = 0; i < blank; ++i)
+        {
+            _tcscat(str, L" ");
+        }
+        for (int i = 0; i < asterisk; ++i)
+        {
+            _tcscat(str, L"*");
+        }
+        _tcscat(str, L"\n");
+        blank += 2;
+        asterisk -= 4;
+    }
+
+    if (asterisk == 0)
+    {
+        for (int i = 0; i < width / 2 - 1; ++i)
+            _tcscat(str, L" ");
+        _tcscat(str, L"**\n");
+        blank -= 2;
+        asterisk += 4;
+    }
+
+    while (asterisk <= width)
+    {
+        for (int i = 0; i < blank; ++i)
+        {
+            _tcscat(str, L" ");
+        }
+        for (int i = 0; i < asterisk; ++i)
+        {
+            _tcscat(str, L"*");
+        }
+        _tcscat(str, L"\n");
+        blank -= 2;
+        asterisk += 4;
+    }
+}
+void drawRectangle(int width, TCHAR *str)
+{
+    for (int i = 0; i < (width / 2); ++i)
+    {
+        if (i == 0 || i == ((width / 2) - 1))
+            for (int j = 0; j < width; ++j)
+                _tcscat(str, L"*");
+        else
+        {
+            _tcscat(str, L"*");
+            for (int j = 0; j < width - 2; ++j)
+                _tcscat(str, L"*");
+            _tcscat(str, L"*");
+        }
+        _tcscat(str, L"\n");
+    }
 }
